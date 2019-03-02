6b105ca1b6b7a79286de46c5264c33f35bb1771a
wmc2tokyo <wmcooper2@yahoo.com>
2019-03-02 12:28:48 +0900

    Removed unecessary dirs.

--- src/unused/thisbookwaswrittenby.py
+++
@@ -1,9 +0,0 @@
-"""Contains lesson specific categories for 'This book was written by Natsume Soseki.
-    Returns None."""
-
-this_book_was_written__nouns = ['book','language','song','word','sentence',
-                             'paragraph','newspaper','magazine','letter',
-                             'love letter','report','response','phrase',
-                             'dictionary','paper','essay','document']
-language_related_past_participles = ['read','written','sold','sung','spoken',
-                                     'taught','understood','written']
