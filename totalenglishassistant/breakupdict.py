71e547dddee469d3a9980741073088fabe21ca0d
wmc2tokyo <wmcooper2@yahoo.com>
2019-06-30 18:41:20 +0900

    Moved data files out of repo and into single, collection on macbook. Made link to data in repo. Updated the wiki and README.md.

---
+++ totalenglishassistant/breakupdict.py
@@ -0,0 +1,21 @@
+"""Break up jhswords.py into text files named after their pages"""
+
+from typing import Text
+import jhsdict as jhs
+
+words = jhs.jhswords
+
+
+
+def save_to(grade: Text, word: Text, num: Text) -> None:
+    """Saves 'word' to the file with name 'num'. Returns None."""
+    file_name = "pages/grade" + grade + "/" + num + ".txt"
+    with open(file_name, "a+") as f:
+        f.write(word+"\n")
+    return None
+
+for word, details in words.items():
+    if details["grade"] not in ["1", "2", "3"]:
+        print(word, "not in any grade")
+    else:
+        save_to(details["grade"], word, details["page"])
